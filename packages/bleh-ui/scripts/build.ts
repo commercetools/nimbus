d3dfb08a550f5ad56ac22f3a9b6fefe3bdffa1dd
Michael Salzmann <michael.salzmann@commercetools.com>
2024-11-24 10:23:49 +0100

    display bundle-size of ui-lib

--- packages/bleh-ui/scripts/build.ts
+++ packages/bleh-ui/scripts/build.ts
@@ -1,8 +1,28 @@
 import dts from "bun-plugin-dts";
+import { statSync } from "fs";
 
-await Bun.build({
+function getFileSizeInKB(filePath: string) {
+  try {
+    const stats = statSync(filePath);
+    const sizeInKB = stats.size / 1024;
+    return sizeInKB.toFixed(2); // returns size in KB with 2 decimal places
+  } catch (error) {
+    console.error("Error reading file size:", error);
+    return null;
+  }
+}
+
+const result = await Bun.build({
   entrypoints: ["./src/index.ts"],
   outdir: "./dist",
   plugins: [dts()],
-  external: ["react", "react-dom"],
+  external: ["react", "react-dom", "@emotion/react"],
+});
+
+result.outputs.forEach((output) => {
+  const { path } = output;
+
+  console.log(path);
+  console.log("=".repeat(path.length));
+  console.log("file-size: ", getFileSizeInKB(path) + "kB");
 });
