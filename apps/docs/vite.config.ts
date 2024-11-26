374e58b711e3957573ef5ee234e94b3d9d055263
Michael Salzmann <michael.salzmann@commercetools.com>
2024-11-26 12:54:16 +0100

    chore: vite: respect config paths

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import fileSystemApiPlugin from "./vite-plugins/vite-plugin-fs-api";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), fileSystemApiPlugin()],
+  plugins: [react(), tsconfigPaths(), fileSystemApiPlugin()],
   build: {
     rollupOptions: {
       output: {
