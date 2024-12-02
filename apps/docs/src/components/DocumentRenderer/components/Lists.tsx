4f02c039cdb8af5490718d6f0e0f92a05b3ebea1
Michael Salzmann <michael.salzmann@commercetools.com>
2024-12-02 14:01:15 +0100

    add some more infos

--- apps/docs/src/components/DocumentRenderer/components/Lists.tsx
+++ apps/docs/src/components/DocumentRenderer/components/Lists.tsx
@@ -20,7 +20,7 @@ UlList.displayName = "UlList";
 export const ListItem = (props) => {
   const { children, ...rest } = props;
   return (
-    <List.Item asChild listStylePosition="outside" marginLeft="5">
+    <List.Item asChild listStylePosition="outside" marginLeft="5" my="1">
       <li {...rest}>
         {Children.map(children, (child) => {
           // Check if the child is a React element and if its type is 'kbd'
