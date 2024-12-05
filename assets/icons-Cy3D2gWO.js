ad0443bcdb9acb8a844c732b5bb6832332d6db67
misama-ct <misama-ct@users.noreply.github.com>
2024-12-05 12:36:15 +0000

    deploy: e9c432c7bd3b8bbbb2b275f322197dabf67e9f1c

--- assets/icons-Cy3D2gWO.js
+++ assets/icons-Cy3D2gWO.js
@@ -1,4 +1,4 @@
-import{r as y}from"./vendor-C-C4Zvbh.js";/**
+import{r as y}from"./vendor-DmZJMPus.js";/**
  * @license lucide-react v0.464.0 - ISC
  *
  * This source code is licensed under the ISC license.
