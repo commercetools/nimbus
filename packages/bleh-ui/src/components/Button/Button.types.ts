fd9e464004bebfe32f43ebe0846da01be0a149a5
Michael Salzmann <michael.salzmann@commercetools.com>
2024-12-09 12:20:56 +0100

    meh

--- packages/bleh-ui/src/components/Button/Button.types.ts
+++ packages/bleh-ui/src/components/Button/Button.types.ts
@@ -2,5 +2,5 @@ import type { ButtonRootProps } from "./Button.slots";
 
 export interface ButtonProps extends ButtonRootProps {
   /** if true, button is busy with something (loading, processing, etc...) */
-  busy: boolean;
+  busy?: boolean;
 }
