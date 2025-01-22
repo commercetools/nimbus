4f9685bcb63facde51f94f68506fca1513ea6a68
Michael Salzmann <michael.salzmann@commercetools.com>
2025-01-22 15:03:00 +0100

    watch m,ode

--- packages/bleh-ui/vite.config.ts
+++ packages/bleh-ui/vite.config.ts
@@ -3,32 +3,48 @@ import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+import dts from "vite-plugin-dts";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
-  build: {
-    lib: {
-      entry: resolve(__dirname, "./src/index.ts"),
-      name: "BlehUI",
-      // the proper extensions will be added
-      fileName: "bleh-ui",
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ["react", "react-dom", "@emotion/react", "@react-types/button"],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          "@emotion/react": "emotionReact",
-          "@react-types/button": "ReactTypesButton",
+export default defineConfig((config) => {
+  const isWatchMode = process.argv.includes("--watch");
+
+  const plugins = [react(), viteTsconfigPaths()];
+
+  if (!isWatchMode) {
+    plugins.push(dts({ rollupTypes: true }));
+  }
+
+  return {
+    plugins,
+    build: {
+      lib: {
+        entry: resolve(__dirname, "./src/index.ts"),
+        name: "BlehUI",
+        // the proper extensions will be added
+        fileName: "bleh-ui",
+      },
+      rollupOptions: {
+        // make sure to externalize deps that shouldn't be bundled
+        // into your library
+        external: [
+          "react",
+          "react-dom",
+          "@emotion/react",
+          "@react-types/button",
+        ],
+        output: {
+          // Provide global variables to use in the UMD build
+          // for externalized deps
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+            "@emotion/react": "emotionReact",
+            "@react-types/button": "ReactTypesButton",
+          },
         },
       },
     },
-  },
+  };
 });
