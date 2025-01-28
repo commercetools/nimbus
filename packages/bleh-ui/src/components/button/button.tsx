79d92eccd7a2cc55702b52f8ab9646e3f918fef1
Ddouglasz <douglasegiemeh@gmail.com>
2025-01-28 15:16:05 +0100

    chore(design system): jest setup restore original button component

--- packages/bleh-ui/src/components/button/button.tsx
+++ packages/bleh-ui/src/components/button/button.tsx
@@ -4,25 +4,19 @@ import { useButton, useObjectRef } from "react-aria";
 import { ButtonRoot } from "./button.slots.tsx";
 import type { ButtonProps } from "./button.types.ts";
 
-// export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-//   (props, ref) => {
-//     const objRef = useObjectRef(ref);
-//     const { buttonProps } = useButton(props, objRef);
-//     const { children, busy, ...rest } = props;
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (props, ref) => {
+    const objRef = useObjectRef(ref);
+    const { buttonProps } = useButton(props, objRef);
+    const { children, busy, ...rest } = props;
 
-//     return (
-//       <ButtonRoot ref={objRef} {...rest} {...buttonProps}>
-//         {children}
-//       </ButtonRoot>
-//     );
-//   }
-// );
+    return (
+      <ButtonRoot ref={objRef} {...rest} {...buttonProps}>
+        {children}
+      </ButtonRoot>
+    );
+  }
+);
 
-// // Manually assign a displayName for debugging purposes
-// Button.displayName = "Button";
-
-export const Button = (props: {children: ReactNode}) => {
-  return <button  data-testid="button1">
-    {props.children}
-  </button>
-}
+// Manually assign a displayName for debugging purposes
+Button.displayName = "Button";
