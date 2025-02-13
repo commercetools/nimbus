3586c1c02c3d9cbda1735d1adb1b8f9934053bc5
Byron Wall <byron.wall@commercetools.com>
2025-02-13 13:40:33 -0500

    feat(tooltip): update jsdoc annotations

--- packages/bleh-ui/src/components/tooltip/tooltip.tsx
+++ packages/bleh-ui/src/components/tooltip/tooltip.tsx
@@ -35,20 +35,20 @@ export const TooltipTrigger = RATooltipTrigger;
  * ============================================================
  * A helper component that adds props from `react-aria`s `useFocusable` hook
  * to its child so that it can be used as a trigger element for a `Tooltip`
- * - [React Aria Components Tooltip Documentation](https://react-spectrum.adobe.com/react-aria/Tooltip.html)
- * - [React Aria Components Issue re:Tooltip with custom trigger](https://github.com/adobe/react-spectrum/issues/5733#issuecomment-1918691983)
- * - [ARIA Tooltip Pattern](https://www.w3.org/TR/wai-aria-1.2/#tooltip)
  *
  * Caveats:
- *
  * - Using non-interactive elements as tooltip triggers is against ARIA best-practices,
  *   it is your responsibility to ensure that the underlying trigger element handles
  *   focus and hover interactions correctly for keyboard-only users
  *
  * Features:
- *
  * - allows forwarding refs to the underlying DOM element
  * - accepts all native html 'HTMLElement' attributes (including aria- & data-attributes)
+ *
+ * Further Context:
+ * - [React Aria Components Tooltip Documentation](https://react-spectrum.adobe.com/react-aria/Tooltip.html)
+ * - [React Aria Components Issue re:Tooltip with custom trigger](https://github.com/adobe/react-spectrum/issues/5733#issuecomment-1918691983)
+ * - [ARIA Tooltip Pattern](https://www.w3.org/TR/wai-aria-1.2/#tooltip)
  */
 export const FocusableTooltipTrigger = forwardRef<
   HTMLElement,
@@ -75,14 +75,16 @@ export const FocusableTooltipTrigger = forwardRef<
  * Tooltip
  * ============================================================
  * A contextual popup that displays a description for an element, uses `Tooltip` component from `react-aria-components`.
- * - [React Aria Components Tooltip Documentation](https://react-spectrum.adobe.com/react-aria/Tooltip.html)
- * - [ARIA Tooltip Pattern](https://www.w3.org/TR/wai-aria-1.2/#tooltip)
  *
  * Features:
  * - allows forwarding refs to the underlying DOM element
  * - accepts all native html 'HTMLSpanElement' attributes (including aria- & data-attributes)
  * - supports 'variants', 'sizes', etc. configured in the recipe
  * - allows overriding styles by using style-props
+ *
+ * Further Context:
+ * - [React Aria Components Tooltip Documentation](https://react-spectrum.adobe.com/react-aria/Tooltip.html)
+ * - [ARIA Tooltip Pattern](https://www.w3.org/TR/wai-aria-1.2/#tooltip)
  */
 
 export const Tooltip = forwardRef<HTMLDivElement, TooltipProps>(
