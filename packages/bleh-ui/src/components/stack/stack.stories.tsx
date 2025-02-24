553c7186da5d851e451994fc652610f689d4d66a
Ddouglasz <douglasegiemeh@gmail.com>
2025-02-24 13:28:56 +0100

    feat(Stack poc): properly represent stacking order and direction

--- packages/bleh-ui/src/components/stack/stack.stories.tsx
+++ packages/bleh-ui/src/components/stack/stack.stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { Box } from "../box";
 import { Stack } from "./stack";
 
 const meta: Meta<typeof Stack> = {
@@ -12,44 +13,70 @@ type Story = StoryObj<typeof Stack>;
 
 export const Basic: Story = {
   args: {
-    children: "Basic Stack",
+    children: [
+      <Box key="1" p="400" bg="primary.7">
+        Item 1
+      </Box>,
+      <Box key="1" p="400" bg="primary.7">
+        Item 2
+      </Box>,
+    ],
   },
 };
 
 export const WithDirection: Story = {
   args: {
     direction: "row",
-    children: (
-      <>
-        <div>Item 1</div>
-        <div>Item 2</div>
-        <div>Item 3</div>
-      </>
-    ),
+    children: [
+      <Box key="1" p="400" bg="primary.7">
+        Item 1
+      </Box>,
+      <Box key="2" p="400" bg="primary.7">
+        Item 2
+      </Box>,
+      <Box key="3" p="400" bg="primary.7">
+        Item 3
+      </Box>,
+    ],
+  },
+  argTypes: {
+    direction: {
+      options: ["row", "column", "row-reverse", "column-reverse"],
+      control: { type: "radio" },
+    },
   },
 };
 
 export const WithSpacing: Story = {
   args: {
-    children: (
-      <>
-        <Stack h="1200">Spaced Item 1</Stack>
-        <Stack h="1200">Spaced Item 2</Stack>
-        <Stack h="1200">Spaced Item 3</Stack>
-      </>
-    ),
+    gap: "100",
+    children: [
+      <Box key="1" p="400" bg="primary.7">
+        Spaced Item 1
+      </Box>,
+      <Box key="1" p="400" bg="primary.7">
+        Spaced Item 2
+      </Box>,
+      <Box key="1" p="400" bg="primary.7">
+        Spaced Item 3
+      </Box>,
+    ],
   },
 };
 
 export const WithSeparator: Story = {
   args: {
     separator: <hr />,
-    children: (
-      <Stack separator={<hr />}>
-        <Stack h="1200">Spaced Item with separator 1</Stack>
-        <Stack h="1200">Spaced Item with separator 2</Stack>
-        <Stack h="1200">Spaced Item with separator 3</Stack>
-      </Stack>
-    ),
+    children: [
+      <Box key="1" p="400" bg="primary.7">
+        Spaced Item with separator 1
+      </Box>,
+      <Box key="1" p="400" bg="primary.7">
+        Spaced Item with separator 2
+      </Box>,
+      <Box key="1" p="400" bg="primary.7">
+        Spaced Item with separator 3
+      </Box>,
+    ],
   },
 };
