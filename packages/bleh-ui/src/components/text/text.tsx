66532259fbc97657c8cbd6eaafb00ea0f27dcdf1
Michael Salzmann <michael.salzmann@commercetools.com>
2025-02-24 09:12:29 +0100

    feat(typography): update Heading and Text component documentation for clarity and order

--- packages/bleh-ui/src/components/text/text.tsx
+++ packages/bleh-ui/src/components/text/text.tsx
@@ -1,1 +1,21 @@
-export { Text } from "@chakra-ui/react";
+import {
+  Text as ChakraText,
+  type TextProps as ChakraTextProps,
+} from "@chakra-ui/react";
+import { forwardRef } from "react";
+
+export interface TextProps extends ChakraTextProps {}
+
+/**
+ * Render Text
+ *
+ * Use this component to render text. Various props can be passed to customize the text.
+ * Check the documentation for more information.
+ *
+ * @see https://DOMAIN/components/typography/text
+ */
+export const Text = forwardRef<HTMLDivElement, TextProps>((props, ref) => {
+  return <ChakraText ref={ref} {...props} />;
+});
+
+Text.displayName = "Text";
