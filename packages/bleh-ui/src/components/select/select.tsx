0629799f8da2a71d8232b1c04c25007315ef5b20
Michael Salzmann <michael.salzmann@commercetools.com>
2025-03-05 09:58:53 +0100

    fix(select): update documentation and examples for Select component with simple examples and prop-tables

--- packages/bleh-ui/src/components/select/select.tsx
+++ packages/bleh-ui/src/components/select/select.tsx
@@ -1,11 +1,18 @@
-import { SelectRoot } from "./components/select.root";
-import { SelectOptions } from "./components/select.options";
-import { SelectOption } from "./components/select.option";
-import { SelectOptionGroup } from "./components/select.option-group";
+import { SelectRoot as _SelectRoot } from "./components/select.root";
+import { SelectOptions as _SelectOptions } from "./components/select.options";
+import { SelectOption as _SelectOption } from "./components/select.option";
+import { SelectOptionGroup as _SelectOptionGroup } from "./components/select.option-group";
 
 export const Select = {
-  Root: SelectRoot,
-  Options: SelectOptions,
-  Option: SelectOption,
-  OptionGroup: SelectOptionGroup,
+  Root: _SelectRoot,
+  Options: _SelectOptions,
+  Option: _SelectOption,
+  OptionGroup: _SelectOptionGroup,
 };
+
+/**
+ * todo: get rid of this, this is needed for the react-docgen-typescript script
+ * that is parsing the typescript types for our documentaiton. The _ underscores
+ * serve as a reminder that this exports are awkward and should not be used.
+ */
+export { _SelectRoot, _SelectOptions, _SelectOption, _SelectOptionGroup };
