4474aa9c7056636d421045349bc2ab99f2929827
Michael Salzmann <michael.salzmann@commercetools.com>
2025-04-08 15:11:05 +0200

    Revert "feat(form-field): refactor context management and simplify FormField components"
    This reverts commit c4f894d627366e7b1ec36c6cee8c21c68f3d64d5.

--- packages/bleh-ui/src/components/form-field/form-field.tsx
+++ packages/bleh-ui/src/components/form-field/form-field.tsx
@@ -1,33 +1,29 @@
 import {
   createContext,
   forwardRef,
-  useCallback,
   useContext,
   useEffect,
   useState,
-  type ReactNode,
+  isValidElement,
+  cloneElement,
+  Children,
 } from "react";
 import {
   FormFieldDescriptionSlot,
   FormFieldErrorSlot,
   FormFieldInputSlot,
   FormFieldLabelSlot,
+  FormFieldPopoverSlot,
   FormFieldRootSlot,
 } from "./form-field.slots";
 import type { FormFieldProps } from "./form-field.types";
+import { ErrorOutline, HelpOutline } from "@bleh-ui/icons";
+import { Box, IconButton } from "@/components";
 
-type FormFieldContextPayloadType = {
-  label?: ReactNode | undefined;
-  description?: ReactNode | undefined;
-  error?: ReactNode | undefined;
-};
+import { Dialog, DialogTrigger, Popover } from "react-aria-components";
+import { useLabel } from "react-aria";
 
-type FormFieldContextType = [
-  FormFieldContextPayloadType,
-  (payload: FormFieldContextPayloadType) => void,
-];
-
-const FormFieldContext = createContext<FormFieldContextType>([{}, () => {}]);
+const FormFieldContext = createContext({});
 
 /**
  * FormField
@@ -35,77 +31,185 @@ const FormFieldContext = createContext<FormFieldContextType>([{}, () => {}]);
  * displays inputs in a FormField context
  */
 export const FormFieldRoot = forwardRef<HTMLDivElement, FormFieldProps>(
-  ({ children, ...props }, ref) => {
-    const [context, setContext] = useState<FormFieldContextPayloadType>({
-      label: "",
-      description: "",
-      error: "",
+  (
+    { isInvalid, isRequired, isDisabled, isReadOnly, children, ...props },
+    ref
+  ) => {
+    const [context, setContext] = useState({
+      label: null,
+      description: null,
+      error: null,
+      info: null,
+      input: null,
+      isInvalid,
+      isRequired,
+      isDisabled,
+      isReadOnly,
     });
 
-    const mergeContexts = useCallback(
-      (pl: object) => {
-        setContext({ ...context, ...pl });
-      },
-      [context]
-    );
+    const { labelProps, fieldProps } = useLabel({
+      label: context.label,
+      ...props,
+    });
+
+    useEffect(() => {
+      setContext((prevContext) => ({
+        ...prevContext,
+        isInvalid,
+        isRequired,
+        isDisabled,
+        isReadOnly,
+      }));
+    }, [isInvalid, isRequired, isDisabled, isReadOnly]);
+
+    const inputProps = {
+      ...fieldProps,
+      isInvalid,
+      isRequired,
+      isDisabled,
+      isReadOnly,
+    };
 
     return (
-      <FormFieldContext.Provider value={[context, mergeContexts]}>
+      <FormFieldContext.Provider value={[context, setContext]}>
         <FormFieldRootSlot ref={ref} {...props}>
           {context.label && (
-            <FormFieldLabelSlot>{context.label}</FormFieldLabelSlot>
+            <FormFieldLabelSlot>
+              <label {...labelProps}>
+                {context.label}
+                {isRequired && <sup aria-hidden="true">*</sup>}
+              </label>
+              {context.info && (
+                <DialogTrigger>
+                  <Box
+                    as="span"
+                    display="inline-block"
+                    position="relative"
+                    width="1ch"
+                    height="1ch"
+                    ml="200"
+                  >
+                    <Box
+                      as="span"
+                      display="flex"
+                      position="absolute"
+                      top="50%"
+                      right="50%"
+                      transform="translate(50%, -50%)"
+                    >
+                      <IconButton
+                        aria-label="__MORE INFO"
+                        size="2xs"
+                        tone="info"
+                        variant="link"
+                      >
+                        <HelpOutline />
+                      </IconButton>
+                    </Box>
+                  </Box>
+                  <Popover>
+                    <FormFieldPopoverSlot asChild>
+                      <Dialog>
+                        <Box p="300">{context.info}</Box>
+                      </Dialog>
+                    </FormFieldPopoverSlot>
+                  </Popover>
+                </DialogTrigger>
+              )}
+            </FormFieldLabelSlot>
           )}
           {context.description && (
             <FormFieldDescriptionSlot>
               {context.description}
             </FormFieldDescriptionSlot>
           )}
-          {context.error && (
-            <FormFieldErrorSlot>{context.error}</FormFieldErrorSlot>
+          {isInvalid && context.error && (
+            <FormFieldErrorSlot>
+              <Box
+                as={ErrorOutline}
+                display="inline-flex"
+                boxSize="400"
+                verticalAlign="text-bottom"
+                mr="100"
+              />
+              {context.error}
+            </FormFieldErrorSlot>
           )}
           {children}
+
+          {context.input && (
+            <FormFieldInputSlot>
+              {Children.map(context.input, (child) => {
+                // Important: Check if the child is a valid React element before cloning.
+                if (isValidElement(child)) {
+                  return cloneElement(child, inputProps);
+                }
+                // If it's not a valid element (e.g., text node, null, undefined), return it as is.
+                return child;
+              })}
+            </FormFieldInputSlot>
+          )}
         </FormFieldRootSlot>
       </FormFieldContext.Provider>
     );
   }
 );
 
-type ContextPayloadType = { children: ReactNode };
-
-const FormFieldLabel = ({ children }: ContextPayloadType) => {
-  const [, setContext] = useContext<FormFieldContextType>(FormFieldContext);
+function FormFieldLabel({ children }) {
+  const [, setContext] = useContext<object>(FormFieldContext);
 
   useEffect(() => {
-    setContext({ label: children });
-  }, [children]);
+    setContext((prevContext) => ({ ...prevContext, label: children }));
+  }, [children, setContext]);
 
   return null;
-};
+}
 
-const FormFieldDescription = ({ children }: ContextPayloadType) => {
-  const [, setContext] = useContext<FormFieldContextType>(FormFieldContext);
+function FormFieldDescription({ children }) {
+  const [, setContext] = useContext(FormFieldContext);
 
   useEffect(() => {
-    setContext({ description: children });
-  }, [children]);
+    setContext((prevContext) => ({ ...prevContext, description: children }));
+  }, [children, setContext]);
 
   return null;
-};
+}
 
-const FormFieldError = ({ children }: ContextPayloadType) => {
-  const [, setContext] = useContext<FormFieldContextType>(FormFieldContext);
+function FormFieldError({ children }) {
+  const [, setContext] = useContext(FormFieldContext);
 
   useEffect(() => {
-    setContext({ error: children });
-  }, [children]);
+    setContext((prevContext) => ({ ...prevContext, error: children }));
+  }, [children, setContext]);
 
   return null;
-};
+}
+
+function FormFieldInfoBox({ children }) {
+  const [, setContext] = useContext(FormFieldContext);
+
+  useEffect(() => {
+    setContext((prevContext) => ({ ...prevContext, info: children }));
+  }, [children, setContext]);
+
+  return null;
+}
+
+function FormFieldInput({ children }) {
+  const [, setContext] = useContext(FormFieldContext);
+
+  useEffect(() => {
+    setContext((prevContext) => ({ ...prevContext, input: children }));
+  }, [children, setContext]);
+
+  return null;
+}
 
 export const FormField = {
   Root: FormFieldRoot,
   Label: FormFieldLabel,
-  Input: FormFieldInputSlot,
+  Input: FormFieldInput,
   Description: FormFieldDescription,
   Error: FormFieldError,
+  InfoBox: FormFieldInfoBox,
 };
