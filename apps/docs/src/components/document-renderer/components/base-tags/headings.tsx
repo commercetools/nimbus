11d431ffda07baefe456571eb65f207f0d5aeb20
Michael Salzmann <michael.salzmann@commercetools.com>
2025-04-16 11:09:23 +0200

    chore: refactor heading components to use common props for slug generation and scroll margin

--- apps/docs/src/components/document-renderer/components/base-tags/headings.tsx
+++ apps/docs/src/components/document-renderer/components/base-tags/headings.tsx
@@ -1,9 +1,24 @@
-import { sluggify } from "@/utils/sluggify";
 import { Heading } from "@commercetools/nimbus";
 import { DetailedHTMLProps, HTMLAttributes, ReactNode } from "react";
+import GithubSlugger from "github-slugger";
 
+const slugger = new GithubSlugger();
+
+/**
+ * Generates a slug from React children.
+ *
+ * This function ensures that the slug generated is consistent with the one produced by the toc-plugin.
+ * It converts the children to a string, generates a slug, and then removes any "-1" suffixes that might
+ * be added by the slugger.
+ *
+ * @param {ReactNode} children - The React children to convert to a slug
+ * @returns {string} The generated slug
+ */
 const sluggifyChildren = (children: ReactNode) => {
-  return sluggify(children ? children.toString() : "");
+  return slugger
+    .slug(children ? children.toString() : "")
+    .split("-1")
+    .join("");
 };
 
 type HeadingProps = DetailedHTMLProps<
@@ -11,49 +26,44 @@ type HeadingProps = DetailedHTMLProps<
   HTMLHeadingElement
 >;
 
+const commonProps = (props: HeadingProps) => ({
+  /**
+   * adds the id to the heading
+   * this is needed for linking directly to this part of the document
+   */
+  id: sluggifyChildren(props.children),
+  /**
+   * makes sure that the heading is not covered by the sticky navbar
+   * magic-number: roughly the header-height + some extra-padding
+   */
+  scrollMarginTop: "2400",
+});
+
 export const H1 = (props: HeadingProps) => (
-  <Heading id={sluggifyChildren(props.children)} mb="600" size="3xl" asChild>
+  <Heading {...commonProps(props)} mb="600" size="3xl" asChild>
     <h1 {...props} />
   </Heading>
 );
 
 export const H2 = (props: HeadingProps) => (
-  <Heading
-    id={sluggifyChildren(props.children)}
-    mb="300"
-    mt="600"
-    size="2xl"
-    asChild
-  >
+  <Heading {...commonProps(props)} mb="300" mt="600" size="2xl" asChild>
     <h2 {...props} />
   </Heading>
 );
 
 export const H3 = (props: HeadingProps) => (
-  <Heading
-    id={sluggifyChildren(props.children)}
-    mb="300"
-    mt="600"
-    size="xl"
-    asChild
-  >
+  <Heading {...commonProps(props)} mb="300" mt="600" size="xl" asChild>
     <h3 {...props} />
   </Heading>
 );
 export const H4 = (props: HeadingProps) => (
-  <Heading
-    id={sluggifyChildren(props.children)}
-    mb="300"
-    mt="600"
-    size="lg"
-    asChild
-  >
+  <Heading {...commonProps(props)} mb="300" mt="600" size="lg" asChild>
     <h4 {...props} />
   </Heading>
 );
 export const H5 = (props: HeadingProps) => (
   <Heading
-    id={sluggifyChildren(props.children)}
+    {...commonProps(props)}
     mb="300"
     mt="600"
     size="lg"
@@ -65,7 +75,7 @@ export const H5 = (props: HeadingProps) => (
 );
 export const H6 = (props: HeadingProps) => (
   <Heading
-    id={sluggifyChildren(props.children)}
+    {...commonProps(props)}
     mb="300"
     mt="600"
     size="lg"
