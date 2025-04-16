4f9db6c07d1a83d586b8cec03b17c0333916233b
Michael Salzmann <michael.salzmann@commercetools.com>
2025-04-16 14:12:07 +0200

    chore: refactor AppNavBarSearch component to use react-aria for accessibility and update dependencies

--- apps/docs/src/components/navigation/app-nav-bar/components/app-nav-bar-search/components/search-result-item.tsx
+++ apps/docs/src/components/navigation/app-nav-bar/components/app-nav-bar-search/components/search-result-item.tsx
@@ -1,44 +1,27 @@
-import { forwardRef } from "react";
-import { SearchResultItemProps } from "../app-nav-bar-search.types";
 import * as icons from "@commercetools/nimbus-icons";
 import { Box, Flex, Heading, Text } from "@commercetools/nimbus";
+import { SearchResultItemProps } from "../app-nav-bar-search";
 
-export const SearchResultItem = forwardRef(
-  ({ item, ...props }: SearchResultItemProps, ref) => {
-    const isSelected = !!props["data-active-item"];
-
-    const IconComponent =
-      (icons[item.icon as keyof typeof icons] as React.ElementType) ||
-      icons.Description;
-
-    const styles = isSelected
-      ? { bg: "primary.9", color: "primary.contrast" }
-      : {};
-
-    return (
-      <Flex px="600" py="300">
-        <Box pr="400" pt="100">
-          <IconComponent size="1.5em" />
-        </Box>
-        <Box
-          display="flex"
-          flexDir="column"
-          gap="0"
-          ref={ref}
-          {...styles}
-          {...props}
-        >
-          <Heading size="lg" truncate>
-            {item.title}{" "}
-          </Heading>
-          <Text mb="100" truncate>
-            {item.description}
-          </Text>
-          <Text my="100" textStyle="xs" truncate opacity={3 / 4}>
-            {item.menu.join(" -> ")}
-          </Text>
-        </Box>
-      </Flex>
-    );
-  }
-);
+export const SearchResultItem = ({ item, ...props }: SearchResultItemProps) => {
+  const IconComponent =
+    (icons[item.icon as keyof typeof icons] as React.ElementType) ||
+    icons.Description;
+  return (
+    <Flex py="300" cursor="pointer" minWidth="0" maxWidth="100%">
+      <Box pr="400" pt="100">
+        <IconComponent size="1.5em" />
+      </Box>
+      <Box minWidth="0" display="flex" flexDir="column" gap="0" {...props}>
+        <Heading size="lg" truncate>
+          {item.title}{" "}
+        </Heading>
+        <Text mb="100" truncate>
+          {item.description}
+        </Text>
+        <Text my="100" textStyle="xs" truncate opacity={3 / 4}>
+          {item.menu.join(" -> ")}
+        </Text>
+      </Box>
+    </Flex>
+  );
+};
