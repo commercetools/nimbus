ac13dd513b5f33aadb0f0fe6f8b739cc99333058
jaikamat <jaipkamat@gmail.com>
2025-04-29 11:14:03 -0400

    chore(docs): get card prop tables to work

--- packages/nimbus/src/components/card/card.tsx
+++ packages/nimbus/src/components/card/card.tsx
@@ -2,6 +2,9 @@ import CardRoot from "./components/card.root";
 import CardHeader from "./components/card.header";
 import CardContent from "./components/card.content";
 
+// Had to do this
+export { CardRoot, CardHeader, CardContent };
+
 export const Card = {
   Root: CardRoot,
   Header: CardHeader,
