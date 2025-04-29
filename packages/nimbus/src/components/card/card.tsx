b8b237f045789526dc4c43b9269a6e566209e48d
jaikamat <jaipkamat@gmail.com>
2025-04-29 11:14:31 -0400

    chore(docs): remove note

--- packages/nimbus/src/components/card/card.tsx
+++ packages/nimbus/src/components/card/card.tsx
@@ -2,7 +2,6 @@ import CardRoot from "./components/card.root";
 import CardHeader from "./components/card.header";
 import CardContent from "./components/card.content";
 
-// Had to do this
 export { CardRoot, CardHeader, CardContent };
 
 export const Card = {
