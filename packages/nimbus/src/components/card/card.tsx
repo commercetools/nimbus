c2dc6f3485099bf171fd8fe1a2fa643a19e3066a
Jai Kamat <jaipkamat@gmail.com>
2025-04-29 11:43:48 -0400

    chore(docs): create `Card` docs (#82)
    * chore(docs): create Card documentation
    
    * chore(docs): first pass at mdx
    
    * chore(docs): second pass over example
    
    * chore(docs): remove images
    
    * chore(docs): fix loading spinner a11y section
    
    * chore(docs): add missed note
    
    * chore(docs): get card prop tables to work
    
    * chore(docs): remove note

--- packages/nimbus/src/components/card/card.tsx
+++ packages/nimbus/src/components/card/card.tsx
@@ -2,6 +2,8 @@ import CardRoot from "./components/card.root";
 import CardHeader from "./components/card.header";
 import CardContent from "./components/card.content";
 
+export { CardRoot, CardHeader, CardContent };
+
 export const Card = {
   Root: CardRoot,
   Header: CardHeader,
