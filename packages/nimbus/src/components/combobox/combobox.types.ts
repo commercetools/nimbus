08591c7943c69d487e34b1f2b07175bbd3868703
Byron Wall <byron.wall@commercetools.com>
2025-04-30 11:38:00 -0400

    feat(combobox): more work i guess

--- packages/nimbus/src/components/combobox/combobox.types.ts
+++ packages/nimbus/src/components/combobox/combobox.types.ts
@@ -1,13 +1,14 @@
-import type { ComboboxRootProps } from "./combobox.slots"
-import type { RecipeVariantProps } from "@chakra-ui/react"
-import { comboboxRecipe } from "./combobox.recipe"
+import type { ComboboxRootProps } from "./combobox.slots";
+import type { RecipeVariantProps } from "@chakra-ui/react";
+import { comboboxRecipe } from "./combobox.recipe";
 
 /**
  * Combines the root props with Chakra UI's recipe variant props.
  * This allows the component to accept both structural props from Root
  * and styling variants from the recipe.
  */
-type ComboboxVariantProps = ComboboxRootProps & RecipeVariantProps<typeof comboboxRecipe>;
+type ComboboxVariantProps = ComboboxRootProps &
+  RecipeVariantProps<typeof comboboxRecipe>;
 
 /**
  * Main props interface for the Combobox component.
