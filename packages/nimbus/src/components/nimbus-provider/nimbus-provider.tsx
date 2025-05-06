3c44caf7b2b0dc240308027101231bdf48afa65a
jaikamat <jaipkamat@gmail.com>
2025-05-06 15:47:14 -0400

    chore(n): remove hook from provider

--- packages/nimbus/src/components/nimbus-provider/nimbus-provider.tsx
+++ packages/nimbus/src/components/nimbus-provider/nimbus-provider.tsx
@@ -6,39 +6,7 @@ import {
 } from "@chakra-ui/react";
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
 import { system } from "../../theme";
-import { useEffect, useState } from "react";
-
-export function useColorScheme() {
-  const [colorScheme, setColorScheme] = useState(getCurrentColorScheme());
-
-  // Helper function to get the current color-scheme from the <html> tag
-  function getCurrentColorScheme() {
-    return (
-      document.documentElement.style.getPropertyValue("color-scheme") ||
-      localStorage.getItem("theme") ||
-      "light"
-    );
-  }
-
-  useEffect(() => {
-    const htmlElement = document.documentElement;
-
-    const observer = new MutationObserver(() => {
-      const newColorScheme = getCurrentColorScheme();
-      setColorScheme(newColorScheme);
-    });
-
-    // Observe changes to the 'style' attribute of the <html> element
-    observer.observe(htmlElement, {
-      attributes: true,
-      attributeFilter: ["style"],
-    });
-
-    return () => observer.disconnect();
-  }, []);
-
-  return colorScheme;
-}
+å;
 
 export function NimbusProvider({ children, ...props }: ColorModeProviderProps) {
   return (
