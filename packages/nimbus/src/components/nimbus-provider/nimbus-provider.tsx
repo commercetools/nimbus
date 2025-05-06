f9cd1c237b8d9c7973e9e7a3598ebacbe69e6261
jaikamat <jaipkamat@gmail.com>
2025-05-06 16:51:53 -0400

    chore(n): remove chakra provider

--- packages/nimbus/src/components/nimbus-provider/nimbus-provider.tsx
+++ packages/nimbus/src/components/nimbus-provider/nimbus-provider.tsx
@@ -1,18 +1,11 @@
 "use client";
 
-import {
-  ChakraProvider,
-  // defaultSystem as defaultSystemChakra,
-} from "@chakra-ui/react";
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
-import { system } from "../../theme";
 
 export function NimbusProvider({ children, ...props }: ColorModeProviderProps) {
   return (
-    <ChakraProvider value={system}>
-      <ColorModeProvider enableSystem={false} {...props}>
-        <>{children}</>
-      </ColorModeProvider>
-    </ChakraProvider>
+    <ColorModeProvider enableSystem={false} {...props}>
+      <>{children}</>
+    </ColorModeProvider>
   );
 }
