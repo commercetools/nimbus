1ac93acef8268f5c96d059d66d15e754e6c47d54
jaikamat <jaipkamat@gmail.com>
2025-05-07 12:27:13 -0400

    chore(nimbus): refactor tag-group to allow for passing of generics in list

--- packages/nimbus/src/components/tag-group/components/tag-group.tag.tsx
+++ packages/nimbus/src/components/tag-group/components/tag-group.tag.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useRef } from "react";
+import { useRef } from "react";
 import { Close as CloseIcon } from "@commercetools/nimbus-icons";
 import { TagGroupTagSlot } from "../tag-group.slots";
 import { ButtonContext, useContextProps, Button } from "react-aria-components";
@@ -6,24 +6,26 @@ import { ButtonContext, useContextProps, Button } from "react-aria-components";
 import type { TagGroupTagComponent } from "../tag-group.types";
 import { IconButton } from "@/components";
 
-export const TagGroupTag: TagGroupTagComponent = forwardRef(
-  ({ children, ...rest }, ref) => {
-    const textValue = typeof children === "string" ? children : undefined;
-    return (
-      <TagGroupTagSlot ref={ref} textValue={textValue} {...rest}>
-        {({ allowsRemoving }) => (
-          <>
-            {children}
-            {allowsRemoving && (
-              <IconButton size="2xs" variant="ghost" slot="remove">
-                <CloseIcon />
-              </IconButton>
-            )}
-          </>
-        )}
-      </TagGroupTagSlot>
-    );
-  }
-);
+export const TagGroupTag: TagGroupTagComponent = ({
+  children,
+  ref,
+  ...rest
+}) => {
+  const textValue = typeof children === "string" ? children : undefined;
+  return (
+    <TagGroupTagSlot ref={ref} textValue={textValue} {...rest}>
+      {({ allowsRemoving }) => (
+        <>
+          {children}
+          {allowsRemoving && (
+            <IconButton size="2xs" variant="ghost" slot="remove">
+              <CloseIcon />
+            </IconButton>
+          )}
+        </>
+      )}
+    </TagGroupTagSlot>
+  );
+};
 
 TagGroupTag.displayName = "TagGroup.Tag";
--- packages/nimbus/src/components/tag-group/tag-group.slots.tsx
+++ packages/nimbus/src/components/tag-group/tag-group.slots.tsx
@@ -1,19 +1,13 @@
 import { createSlotRecipeContext } from "@chakra-ui/react";
-import {
-  TagGroup as RaTagGroup,
-  TagList as RaTagList,
-  Tag as RaTag,
-} from "react-aria-components";
+import { TagGroup as RaTagGroup, Tag as RaTag } from "react-aria-components";
 
 import type {
   TagGroupProps,
-  TagGroupTagListComponent,
-  TagGroupTagListProps,
   TagGroupTagComponent,
   TagGroupTagProps,
 } from "./tag-group.types";
 
-const { withContext, withProvider } = createSlotRecipeContext({
+export const { withContext, withProvider } = createSlotRecipeContext({
   key: "taggroup",
 });
 
@@ -21,12 +15,6 @@ export const TagGroupRootSlot = withProvider<typeof RaTagGroup, TagGroupProps>(
   RaTagGroup,
   "root"
 );
-
-export const TagGroupTagListSlot: TagGroupTagListComponent<object> =
-  withContext<typeof RaTagList, TagGroupTagListProps<object>>(
-    RaTagList,
-    "tagList"
-  );
 
 export const TagGroupTagSlot: TagGroupTagComponent = withContext<
   typeof RaTag,
