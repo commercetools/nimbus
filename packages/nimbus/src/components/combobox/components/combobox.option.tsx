807e77b1aae8a76a05cc0a752a21a36f0be6da7c
jaikamat <jaipkamat@gmail.com>
2025-05-14 14:14:57 -0400

    chore(nimbus): get the dang thing to work, type safe

--- packages/nimbus/src/components/combobox/components/combobox.option.tsx
+++ packages/nimbus/src/components/combobox/components/combobox.option.tsx
@@ -2,15 +2,23 @@ import { type RefAttributes } from "react";
 
 import { ComboBoxOptionSlot } from "../combobox.slots";
 import type { ComboBoxOptionProps } from "../combobox.types";
+import { ListBoxItem } from "react-aria-components";
+import { extractStyleProps } from "@/utils/extractStyleProps";
 
 export const ComboBoxOption = <T extends object>({
   children,
   ref,
   ...props
-}: ComboBoxOptionProps<T> & RefAttributes<HTMLDivElement>) => (
-  <ComboBoxOptionSlot ref={ref} {...props}>
-    {children}
-  </ComboBoxOptionSlot>
-);
+}: ComboBoxOptionProps<T> & RefAttributes<HTMLDivElement>) => {
+  const [styleProps, restProps] = extractStyleProps(props);
+
+  return (
+    <ComboBoxOptionSlot asChild>
+      <ListBoxItem ref={ref} {...restProps} {...styleProps}>
+        {children}
+      </ListBoxItem>
+    </ComboBoxOptionSlot>
+  );
+};
 
 ComboBoxOption.displayName = "ComboBox.Option";
