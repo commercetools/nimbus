e2b151c5c6304a010c46d3ad903d2ebb2f774282
Byron Wall <byron.wall@commercetools.com>
2025-05-15 13:02:45 -0400

    feat(combobox): single select combobox implemented

--- packages/nimbus/src/components/combobox/components/combobox.options.tsx
+++ packages/nimbus/src/components/combobox/components/combobox.options.tsx
@@ -1,25 +1,25 @@
-import { type RefAttributes, useContext } from "react";
-import { ComboBoxStateContext, ListBox } from "react-aria-components";
+import { type ForwardedRef } from "react";
+import { ListBox as RaListBox } from "react-aria-components";
 import { ComboBoxOptionsSlot } from "../combobox.slots";
 import type { ComboBoxOptionsProps } from "../combobox.types";
 import { extractStyleProps } from "@/utils/extractStyleProps";
+import { fixedForwardRef } from "@/utils/fixedForwardRef";
 
-export const ComboBoxOptions = <T extends object>({
-  children,
-  ref,
-  ...props
-}: ComboBoxOptionsProps<T> & RefAttributes<HTMLDivElement>) => {
-  const [styleProps, restProps] = extractStyleProps(props);
+export const ComboBoxOptions = fixedForwardRef(
+  <T extends object>(
+    { children, ...props }: ComboBoxOptionsProps<T>,
+    ref: ForwardedRef<HTMLDivElement>
+  ) => {
+    const [styleProps, restProps] = extractStyleProps(props);
+    return (
+      <ComboBoxOptionsSlot {...styleProps} asChild>
+        <RaListBox ref={ref} {...restProps}>
+          {children}
+        </RaListBox>
+      </ComboBoxOptionsSlot>
+    );
+  }
+);
 
-  const state = useContext(ComboBoxStateContext);
-  console.log(state);
-  return (
-    <ComboBoxOptionsSlot asChild>
-      <ListBox ref={ref} {...restProps} {...styleProps}>
-        {children}
-      </ListBox>
-    </ComboBoxOptionsSlot>
-  );
-};
-
+// @ts-expect-error - doesn't work with this complex types
 ComboBoxOptions.displayName = "ComboBox.Options";
