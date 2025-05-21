58087bcea0e95e6aa68cc8f8ca1221d2731b1f1f
Michael Salzmann <michael.salzmann@commercetools.com>
2025-05-21 11:43:03 +0200

    refactor(list): update list component documentation and exports
    - Changed the documentation in list.mdx to indicate the component is experimental and not for production use.
    - Simplified the exports in list.tsx by directly exporting the List component from @chakra-ui/react, removing unnecessary wrapper components.

--- packages/nimbus/src/components/list/list.tsx
+++ packages/nimbus/src/components/list/list.tsx
@@ -1,9 +1,1 @@
-import { List } from "@chakra-ui/react";
-
-export type ListRootType = React.ComponentProps<typeof List.Root>;
-export type ListItemType = React.ComponentProps<typeof List.Item>;
-
-const ListRoot = (props: ListRootType) => <List.Root {...props} />;
-const ListItem = (props: ListItemType) => <List.Item {...props} />;
-
-export { List, ListRoot, ListItem };
+export { List } from "@chakra-ui/react";
