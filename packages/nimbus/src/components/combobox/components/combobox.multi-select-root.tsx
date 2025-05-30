cabc173c3bfb2c30932addcd32748849984be22c
jaikamat <jaipkamat@gmail.com>
2025-05-30 11:14:34 -0400

    chore(nimbus): remove forwardRef in combobox

--- packages/nimbus/src/components/combobox/components/combobox.multi-select-root.tsx
+++ packages/nimbus/src/components/combobox/components/combobox.multi-select-root.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, type ForwardedRef, type KeyboardEvent } from "react";
+import { useState, useRef, type KeyboardEvent } from "react";
 import { useSlotRecipe } from "@chakra-ui/react";
 import {
   Popover as RaPopover,
@@ -19,7 +19,6 @@ import { comboBoxSlotRecipe } from "../combobox.recipe";
 import { ComboBoxRootSlot } from "../combobox.slots";
 import type { ComboBoxMultiSelectRootProps } from "../combobox.types";
 
-import { fixedForwardRef } from "@/utils/fixedForwardRef";
 import { extractStyleProps } from "@/utils/extractStyleProps";
 
 function getLastValueInSet(set: Set<Key>) {
@@ -28,130 +27,125 @@ function getLastValueInSet(set: Set<Key>) {
   return value;
 }
 
-export const MultiSelectRoot = fixedForwardRef(
-  <T extends object>(
-    {
-      children,
-      defaultFilter,
-      inputValue: inputValueProp,
-      onInputChange: onInputChangeProp,
-      defaultSelectedKeys = new Set(),
-      selectedKeys: selectedKeysProp,
-      onSelectionChange: onSelectionChangeProp,
-      disabledKeys,
-      items,
-      itemID = "id",
-      itemValue = "name",
-      ...props
-    }: ComboBoxMultiSelectRootProps<T>,
-    ref: ForwardedRef<HTMLDivElement>
-  ) => {
-    const recipe = useSlotRecipe({ recipe: comboBoxSlotRecipe });
-    const [recipeProps, restRecipeProps] = recipe.splitVariantProps(props);
-    const [styleProps] = extractStyleProps(restRecipeProps);
+export const MultiSelectRoot = <T extends object>({
+  children,
+  defaultFilter,
+  inputValue: inputValueProp,
+  onInputChange: onInputChangeProp,
+  defaultSelectedKeys = new Set(),
+  selectedKeys: selectedKeysProp,
+  onSelectionChange: onSelectionChangeProp,
+  disabledKeys,
+  items,
+  itemID = "id",
+  itemValue = "name",
+  ref,
+  ...props
+}: ComboBoxMultiSelectRootProps<T>) => {
+  const recipe = useSlotRecipe({ recipe: comboBoxSlotRecipe });
+  const [recipeProps, restRecipeProps] = recipe.splitVariantProps(props);
+  const [styleProps] = extractStyleProps(restRecipeProps);
 
-    const [_selectedKeys, _setSelectedKeys] = useState<Selection>();
-    const selectedKeys =
-      selectedKeysProp ?? _selectedKeys ?? defaultSelectedKeys;
-    const setSelectedKeys = onSelectionChangeProp ?? _setSelectedKeys;
+  const [_selectedKeys, _setSelectedKeys] = useState<Selection>();
+  const selectedKeys = selectedKeysProp ?? _selectedKeys ?? defaultSelectedKeys;
+  const setSelectedKeys = onSelectionChangeProp ?? _setSelectedKeys;
 
-    const [_inputValue, _setInputValue] = useState<string>("");
-    const inputValue = inputValueProp ?? _inputValue;
-    const setInputValue = onInputChangeProp ?? _setInputValue;
+  const [_inputValue, _setInputValue] = useState<string>("");
+  const inputValue = inputValueProp ?? _inputValue;
+  const setInputValue = onInputChangeProp ?? _setInputValue;
 
-    const deleteLastSelectedItem = () => {
-      // Only handle if selectedKeys is a Set (not "all")
-      if (selectedKeys !== "all") {
-        const lastKey = getLastValueInSet(selectedKeys as Set<Key>);
-        if (lastKey !== null && lastKey !== undefined) {
-          setSelectedKeys(
-            (selectedKeys as Set<Key>).difference(new Set([lastKey]))
-          );
-        }
+  const deleteLastSelectedItem = () => {
+    // Only handle if selectedKeys is a Set (not "all")
+    if (selectedKeys !== "all") {
+      const lastKey = getLastValueInSet(selectedKeys as Set<Key>);
+      if (lastKey !== null && lastKey !== undefined) {
+        setSelectedKeys(
+          (selectedKeys as Set<Key>).difference(new Set([lastKey]))
+        );
       }
-    };
+    }
+  };
 
-    const handleInputKeyDown = (e: KeyboardEvent) => {
-      console.log(e.key, inputValue);
-      if (e.key === "Backspace" && inputValue === "") {
-        deleteLastSelectedItem();
-      }
-    };
+  const handleInputKeyDown = (e: KeyboardEvent) => {
+    console.log(e.key, inputValue);
+    if (e.key === "Backspace" && inputValue === "") {
+      deleteLastSelectedItem();
+    }
+  };
 
-    const filterUtils = useFilter({ sensitivity: "base" });
-    const contains = (...args: Parameters<typeof filterUtils.contains>) =>
-      filterUtils.contains.apply(undefined, args);
+  const filterUtils = useFilter({ sensitivity: "base" });
+  const contains = (...args: Parameters<typeof filterUtils.contains>) =>
+    filterUtils.contains.apply(undefined, args);
 
-    const triggerRef = useRef<HTMLDivElement>(null);
-    const scrollRef = useRef<HTMLDivElement>(null);
+  const triggerRef = useRef<HTMLDivElement>(null);
+  const scrollRef = useRef<HTMLDivElement>(null);
 
-    //TODO:
-    // - mechanism to know which item key to use in the tag to display the selected item
-    // - better control of opening/closing the popover - up/down keys trigger open, open on focus of down arrow button, etc
-    // - types in general, esp around items, etc
-    // - sections: will they even work here?
-    // - styling: input in popover, better focus styling on options, etc
-    // - props for empty states for the tag group and listbox
-    // - resize the popover body to match the input width
-    // - disabled, read only, etc states
-    return (
-      <ComboBoxRootSlot
-        selectionMode="multiple"
-        asChild
-        ref={ref}
-        {...recipeProps}
-        {...styleProps}
-      >
-        <RaGroup ref={triggerRef}>
-          <RaDialogTrigger>
-            <MultiSelectValue
-              items={items}
-              selectedKeys={selectedKeys}
-              onSelectionChange={setSelectedKeys}
-              itemID={itemID}
-              itemValue={itemValue}
-            />
-            <ComboBoxButtonGroup
-              selectedKeys={selectedKeys}
-              onSelectionChange={setSelectedKeys}
+  //TODO:
+  // - mechanism to know which item key to use in the tag to display the selected item
+  // - better control of opening/closing the popover - up/down keys trigger open, open on focus of down arrow button, etc
+  // - types in general, esp around items, etc
+  // - sections: will they even work here?
+  // - styling: input in popover, better focus styling on options, etc
+  // - props for empty states for the tag group and listbox
+  // - resize the popover body to match the input width
+  // - disabled, read only, etc states
+  return (
+    <ComboBoxRootSlot
+      selectionMode="multiple"
+      asChild
+      ref={ref}
+      {...recipeProps}
+      {...styleProps}
+    >
+      <RaGroup ref={triggerRef}>
+        <RaDialogTrigger>
+          <MultiSelectValue
+            items={items}
+            selectedKeys={selectedKeys}
+            onSelectionChange={setSelectedKeys}
+            itemID={itemID}
+            itemValue={itemValue}
+          />
+          <ComboBoxButtonGroup
+            selectedKeys={selectedKeys}
+            onSelectionChange={setSelectedKeys}
+            onInputChange={setInputValue}
+          />
+          <RaPopover
+            triggerRef={triggerRef}
+            scrollRef={scrollRef}
+            placement="bottom start"
+          >
+            <RaAutocomplete
+              aria-label="nimbus-combobox-autocomplete"
+              filter={defaultFilter ?? contains}
+              inputValue={inputValue}
               onInputChange={setInputValue}
-            />
-            <RaPopover
-              triggerRef={triggerRef}
-              scrollRef={scrollRef}
-              placement="bottom start"
+              {...props}
             >
-              <RaAutocomplete
-                aria-label="nimbus-combobox-autocomplete"
-                filter={defaultFilter ?? contains}
-                inputValue={inputValue}
-                onInputChange={setInputValue}
-                {...props}
+              <TextField aria-label="combobox input">
+                <RaInput
+                  autoFocus
+                  onKeyDownCapture={handleInputKeyDown}
+                  placeholder="search..."
+                />
+              </TextField>
+              <ComboBoxOptions
+                ref={scrollRef}
+                items={items}
+                selectionMode={"multiple"}
+                onSelectionChange={setSelectedKeys}
+                selectedKeys={selectedKeys}
+                shouldFocusWrap={true}
+                disabledKeys={disabledKeys}
+                escapeKeyBehavior="none"
               >
-                <TextField aria-label="combobox input">
-                  <RaInput
-                    autoFocus
-                    onKeyDownCapture={handleInputKeyDown}
-                    placeholder="search..."
-                  />
-                </TextField>
-                <ComboBoxOptions
-                  ref={scrollRef}
-                  items={items}
-                  selectionMode={"multiple"}
-                  onSelectionChange={setSelectedKeys}
-                  selectedKeys={selectedKeys}
-                  shouldFocusWrap={true}
-                  disabledKeys={disabledKeys}
-                  escapeKeyBehavior="none"
-                >
-                  {children}
-                </ComboBoxOptions>
-              </RaAutocomplete>
-            </RaPopover>
-          </RaDialogTrigger>
-        </RaGroup>
-      </ComboBoxRootSlot>
-    );
-  }
-);
+                {children}
+              </ComboBoxOptions>
+            </RaAutocomplete>
+          </RaPopover>
+        </RaDialogTrigger>
+      </RaGroup>
+    </ComboBoxRootSlot>
+  );
+};
--- packages/nimbus/src/components/combobox/components/combobox.root.tsx
+++ packages/nimbus/src/components/combobox/components/combobox.root.tsx
@@ -1,4 +1,3 @@
-import { type ForwardedRef } from "react";
 import { useSlotRecipe } from "@chakra-ui/react";
 import { comboBoxSlotRecipe } from "../combobox.recipe";
 import { ComboBoxRootSlot } from "../combobox.slots";
@@ -6,30 +5,27 @@ import { SingleSelectRoot } from "./combobox.single-select-root";
 import { MultiSelectRoot } from "./combobox.multi-select-root";
 import type { ComboBoxRootProps } from "../combobox.types";
 import { extractStyleProps } from "@/utils/extractStyleProps";
-import { fixedForwardRef } from "@/utils/fixedForwardRef";
 
-export const ComboBoxRoot = fixedForwardRef(
-  <T extends object>(
-    { children, ...props }: ComboBoxRootProps<T>,
-    ref: ForwardedRef<HTMLDivElement>
-  ) => {
-    const recipe = useSlotRecipe({ recipe: comboBoxSlotRecipe });
-    const [recipeProps, restRecipeProps] = recipe.splitVariantProps(props);
-    const [styleProps, restProps] = extractStyleProps(restRecipeProps);
-    const Component =
-      "selectionMode" in props && props.selectionMode === "multiple"
-        ? MultiSelectRoot
-        : SingleSelectRoot;
+export const ComboBoxRoot = <T extends object>({
+  children,
+  ref,
+  ...props
+}: ComboBoxRootProps<T>) => {
+  const recipe = useSlotRecipe({ recipe: comboBoxSlotRecipe });
+  const [recipeProps, restRecipeProps] = recipe.splitVariantProps(props);
+  const [styleProps, restProps] = extractStyleProps(restRecipeProps);
+  const Component =
+    "selectionMode" in props && props.selectionMode === "multiple"
+      ? MultiSelectRoot
+      : SingleSelectRoot;
 
-    return (
-      <ComboBoxRootSlot asChild ref={ref} {...recipeProps} {...styleProps}>
-        <Component {...restProps} ref={ref}>
-          {children}
-        </Component>
-      </ComboBoxRootSlot>
-    );
-  }
-);
+  return (
+    <ComboBoxRootSlot asChild ref={ref} {...recipeProps} {...styleProps}>
+      <Component {...restProps} ref={ref}>
+        {children}
+      </Component>
+    </ComboBoxRootSlot>
+  );
+};
 
-// @ts-expect-error - doesn't work with this complex types
 ComboBoxRoot.displayName = "ComboBox.Root";
