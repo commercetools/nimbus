66688c7ff85e80b53fdeba4ece515ecadb82c1d5
Michael Salzmann <michael.salzmann@commercetools.com>
2025-05-30 13:08:25 +0200

    Revert "chore(time-input): remove I18nProvider decorator from TimeInput stories"
    This reverts commit 7435419aac520527848a971b1f211561d3263355.

--- packages/nimbus/src/components/time-input/time-input.stories.tsx
+++ packages/nimbus/src/components/time-input/time-input.stories.tsx
@@ -20,6 +20,13 @@ const inventionOfTheInternet = parseZonedDateTime(
 const meta: Meta<typeof TimeInput> = {
   title: "components/TimeInput",
   component: TimeInput,
+  decorators: [
+    (Story) => (
+      <I18nProvider locale="en-US">
+        <Story />
+      </I18nProvider>
+    ),
+  ],
 };
 
 export default meta;
