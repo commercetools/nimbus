ef48e90992477fe83a0c5f10ab6da389d7db1861
Michael Salzmann <michael.salzmann@commercetools.com>
2025-05-30 12:53:40 +0200

    chore(time-input): remove I18nProvider decorator from TimeInput stories

--- packages/nimbus/src/components/time-input/time-input.stories.tsx
+++ packages/nimbus/src/components/time-input/time-input.stories.tsx
@@ -20,13 +20,6 @@ const inventionOfTheInternet = parseZonedDateTime(
 const meta: Meta<typeof TimeInput> = {
   title: "components/TimeInput",
   component: TimeInput,
-  decorators: [
-    (Story) => (
-      <I18nProvider locale="en-US">
-        <Story />
-      </I18nProvider>
-    ),
-  ],
 };
 
 export default meta;
