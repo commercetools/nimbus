90d51efa4698da1385cfe3ca176bbbd5ce1fc642
Michael Salzmann <michael.salzmann@commercetools.com>
2025-06-12 13:33:43 +0200

    feat(calendar): enhance Calendar component with recipe-based styling and prop extraction
    - Integrated Chakra UI's recipe system for styling the Calendar component.
    - Added functionality to split variant and style props for improved customization.
    - Updated the CalendarRootSlot to utilize the new recipe and style props, enhancing modularity and design consistency.

--- packages/nimbus/src/components/calendar/calendar.tsx
+++ packages/nimbus/src/components/calendar/calendar.tsx
@@ -6,11 +6,18 @@ import { Calendar as RaCalendar } from "react-aria-components";
 import { CalendarRootSlot } from "./calendar.slots";
 import { CalendarGrids } from "./components/calendar.grids";
 import { CalendarHeader } from "./components/calendar.header";
+import { calendarSlotRecipe } from "./calendar.recipe";
+import { useRecipe } from "@chakra-ui/react";
+import { extractStyleProps } from "@/utils/extractStyleProps";
 
 export const Calendar = (props: CalendarProps<DateValue>) => {
+  const recipe = useRecipe({ recipe: calendarSlotRecipe });
+  const [recipeProps, remainingProps] = recipe.splitVariantProps(props);
+  const [styleProps, otherProps] = extractStyleProps(remainingProps);
+
   return (
-    <CalendarRootSlot asChild>
-      <RaCalendar {...props}>
+    <CalendarRootSlot {...recipeProps} {...styleProps} asChild>
+      <RaCalendar {...otherProps}>
         <CalendarHeader />
         <CalendarGrids />
       </RaCalendar>
