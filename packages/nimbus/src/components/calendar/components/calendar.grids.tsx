e08f03d126196f60339640680687d623bfb24af6
Michael Salzmann <michael.salzmann@commercetools.com>
2025-06-16 12:01:07 +0200

    feat(calendar): add month title display to calendar grids
    - Introduced a new month title slot in the calendar component to display the name of each month above the corresponding grid.
    - Updated CalendarGrids to conditionally render month titles based on the number of visible months, enhancing the calendar's usability and visual clarity.

--- packages/nimbus/src/components/calendar/components/calendar.grids.tsx
+++ packages/nimbus/src/components/calendar/components/calendar.grids.tsx
@@ -1,3 +1,5 @@
+import { useContext } from "react";
+import { today, getLocalTimeZone } from "@internationalized/date";
 import {
   CalendarGrid as RaCalendarGrid,
   CalendarGridBody as RaCalendarGridBody,
@@ -6,8 +8,12 @@ import {
   CalendarCell as RaCalendarCell,
   useSlottedContext,
   CalendarContext,
+  CalendarStateContext,
+  useLocale,
 } from "react-aria-components";
-import { today, getLocalTimeZone } from "@internationalized/date";
+
+import { Box } from "@/components";
+
 import {
   CalendarCellSlot,
   CalendarGridBodySlot,
@@ -15,42 +21,61 @@ import {
   CalendarGridSlot,
   CalendarGridsSlot,
   CalendarHeaderCellSlot,
+  CalendarMonthTitleSlot,
 } from "../calendar.slots";
 
 export const CalendarGrids = () => {
+  const { locale } = useLocale();
   const context = useSlottedContext(CalendarContext);
-  const arr = new Array(context?.visibleDuration?.months || 1).fill("");
+  const calendarState = useContext(CalendarStateContext)!;
+  const visibleMonths = context?.visibleDuration?.months || 1;
+  const arr = new Array(visibleMonths).fill("");
   const todayDate = today(getLocalTimeZone());
+  const showMonthTitles = visibleMonths > 1;
 
   return (
     <CalendarGridsSlot>
-      {arr.map((_, index) => (
-        <CalendarGridSlot asChild>
-          <RaCalendarGrid offset={{ months: index }} weekdayStyle="short">
-            <CalendarGridHeaderSlot asChild>
-              <RaCalendarGridHeader>
-                {(day) => (
-                  <CalendarHeaderCellSlot asChild>
-                    <RaCalendarHeaderCell>{day}</RaCalendarHeaderCell>
-                  </CalendarHeaderCellSlot>
-                )}
-              </RaCalendarGridHeader>
-            </CalendarGridHeaderSlot>
-            <CalendarGridBodySlot asChild>
-              <RaCalendarGridBody>
-                {(date) => {
-                  const isToday = date.compare(todayDate) === 0;
-                  return (
-                    <CalendarCellSlot asChild data-today={isToday}>
-                      <RaCalendarCell date={date} />
-                    </CalendarCellSlot>
-                  );
-                }}
-              </RaCalendarGridBody>
-            </CalendarGridBodySlot>
-          </RaCalendarGrid>
-        </CalendarGridSlot>
-      ))}
+      {arr.map((_, index) => {
+        const gridDate = calendarState.visibleRange.start.add({
+          months: index,
+        });
+        const monthName = new Intl.DateTimeFormat(locale, {
+          month: "long",
+        }).format(gridDate.toDate(getLocalTimeZone()));
+
+        return (
+          <Box key={monthName}>
+            {showMonthTitles && (
+              <CalendarMonthTitleSlot>{monthName}</CalendarMonthTitleSlot>
+            )}
+            <CalendarGridSlot asChild>
+              <RaCalendarGrid offset={{ months: index }} weekdayStyle="short">
+                <CalendarGridHeaderSlot asChild>
+                  <RaCalendarGridHeader>
+                    {(day) => (
+                      <CalendarHeaderCellSlot asChild>
+                        <RaCalendarHeaderCell>{day}</RaCalendarHeaderCell>
+                      </CalendarHeaderCellSlot>
+                    )}
+                  </RaCalendarGridHeader>
+                </CalendarGridHeaderSlot>
+                <CalendarGridBodySlot asChild>
+                  <RaCalendarGridBody>
+                    {(date) => {
+                      const isToday = date.compare(todayDate) === 0;
+                      return (
+                        <CalendarCellSlot asChild data-today={isToday}>
+                          <RaCalendarCell date={date} />
+                        </CalendarCellSlot>
+                      );
+                    }}
+                  </RaCalendarGridBody>
+                </CalendarGridBodySlot>
+              </RaCalendarGrid>
+            </CalendarGridSlot>
+          </Box>
+        );
+      })}
     </CalendarGridsSlot>
   );
 };
