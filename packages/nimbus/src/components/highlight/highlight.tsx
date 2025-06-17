d0bcedfcc1f25c8b391d7c4f07f20e7ce473c69f
Michael Salzmann <michael.salzmann@commercetools.com>
2025-06-17 14:39:19 +0200

    refactor(highlight): remove Highlight component and related files
    - Deleted the Highlight component and its associated documentation and index files from the Nimbus package.
    - Updated the index.ts file to exclude the Highlight export, streamlining the component exports.

--- packages/nimbus/src/components/highlight/highlight.tsx
+++
@@ -1,6 +0,0 @@
-import { Highlight as ChakraHighlight } from "@chakra-ui/react";
-
-/**
- * @experimental This component is experimental and may change or be removed in future versions.
- */
-export const Highlight = ChakraHighlight;
