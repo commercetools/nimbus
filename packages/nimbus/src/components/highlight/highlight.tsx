543f28220278cef63a64e960099004d03b81f563
Michael Salzmann <michael.salzmann@commercetools.com>
2025-06-17 14:21:52 +0200

    feat(lifecycle): mark additional components as experimental
    - Updated the lifecycle state for the Code, Dialog, Highlight, Kbd, and List components to 'Experimental'.
    - Added documentation comments indicating that these components may change or be removed in future versions.

--- packages/nimbus/src/components/highlight/highlight.tsx
+++ packages/nimbus/src/components/highlight/highlight.tsx
@@ -1,1 +1,6 @@
-export { Highlight } from "@chakra-ui/react";
+import { Highlight as ChakraHighlight } from "@chakra-ui/react";
+
+/**
+ * @experimental This component is experimental and may change or be removed in future versions.
+ */
+export const Highlight = ChakraHighlight;
