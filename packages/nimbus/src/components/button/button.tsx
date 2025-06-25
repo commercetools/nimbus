638f35ce14934d431be77f5add443acc1c0aaf30
Michael Salzmann <michael.salzmann@commercetools.com>
2025-06-25 13:24:32 +0200

    chore(button): revert button changes

--- packages/nimbus/src/components/button/button.tsx
+++ packages/nimbus/src/components/button/button.tsx
@@ -1,6 +1,5 @@
 import { useRef } from "react";
 import { useButton, useObjectRef, mergeProps } from "react-aria";
-import { ButtonContext, useContextProps } from "react-aria-components";
 import { mergeRefs } from "@chakra-ui/react";
 import { ButtonRoot } from "./button.slots.tsx";
 import type { ButtonProps } from "./button.types.ts";
@@ -13,9 +12,6 @@ export const Button = (props: ButtonProps) => {
   // merge the local ref with a potentially forwarded ref
   const ref = useObjectRef(mergeRefs(localRef, forwardedRef));
 
-  // Consume context props based on slot (this enables slot-aware behavior)
-  const [contextProps] = useContextProps(rest, ref, ButtonContext);
-
   // if asChild is set, for react-aria to add the button-role, the elementType
   // has to be manually set to something else than button
 
@@ -23,17 +19,14 @@ export const Button = (props: ButtonProps) => {
 
   const { buttonProps } = useButton(
     {
-      ...contextProps,
+      ...rest,
       elementType,
     },
     ref
   );
 
   return (
-    <ButtonRoot
-      {...mergeProps(contextProps, buttonProps, { as, asChild })}
-      ref={ref}
-    >
+    <ButtonRoot {...mergeProps(rest, buttonProps, { as, asChild })} ref={ref}>
       {children}
     </ButtonRoot>
   );
