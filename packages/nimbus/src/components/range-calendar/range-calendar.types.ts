5d341dcfbda0e6a55a6b12bd5073a8eab7a9ca20
Valorie <valorie.carli@commercetools.com>
2025-07-08 14:20:51 -0400

    chore(date-range-input): clean up and add stories to range calendar

--- packages/nimbus/src/components/range-calendar/range-calendar.types.ts
+++ packages/nimbus/src/components/range-calendar/range-calendar.types.ts
@@ -5,10 +5,18 @@ import type { rangeCalendarSlotRecipe } from "./range-calendar.recipe";
 import type { RangeCalendarRootSlotProps } from "./range-calendar.slots";
 
 /**
- * Additional properties we want to exclude from the TimeInput component.
+ * Range value type for date ranges
+ */
+export type RangeValue<T> = {
+  start: T;
+  end: T;
+};
+
+/**
+ * Additional properties we want to exclude from the RangeCalendar component.
  * These are either deprecated or not intended for use in this component.
  */
-type ExcludedProps = "as" | "asChild" | "style" | "createCalendar";
+type ExcludedProps = "as" | "style" | "createCalendar";
 
 export interface RangeCalendarProps<T extends DateValue>
   extends Omit<
