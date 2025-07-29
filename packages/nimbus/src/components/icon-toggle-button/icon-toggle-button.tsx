4ac5272a3b0b3fa85182dfb21954fbd015e7a2c0
Michael Salzmann <michael.salzmann@commercetools.com>
2025-07-29 11:25:18 +0200

    CRAFT-1644: ToggleButton & IconToggleButton (#293)
    * feat(toggle-button): add ToggleButton & IconToggleButton components
    
    * refactor(toggle-button): exclude additional chakra-ui props from ToggleButton component
    
    * feat(toggle-button): add documentation for ToggleButton component with usage examples and accessibility guidelines
    
    * refactor(toggle-button): simplify toggle button recipe and enhance styling variables
    
    * feat(toggle-button): update icon in stories and add ref support for ToggleButton component
    
    * feat(icon-toggle-button): add IconToggleButton component with documentation and accessibility features
    
    * refactor(icon-toggle-button): simplify stories by removing unused props and consolidating variants

---
+++ packages/nimbus/src/components/icon-toggle-button/icon-toggle-button.tsx
@@ -0,0 +1,24 @@
+import { ToggleButton } from "@/components/toggle-button";
+import type { IconToggleButtonProps } from "./icon-toggle-button.types";
+
+/**
+ * # IconToggleButton
+ * ============================================================
+ * A toggle button with only an icon as child. It is based
+ * on the regular `ToggleButton` component, but with a few adjustments.
+ *
+ */
+export const IconToggleButton = ({
+  "aria-label": ariaLabel,
+  children,
+  ref,
+  ...props
+}: IconToggleButtonProps) => {
+  return (
+    <ToggleButton ref={ref} aria-label={ariaLabel} px={0} py={0} {...props}>
+      {children}
+    </ToggleButton>
+  );
+};
+
+IconToggleButton.displayName = "IconToggleButton";
---
+++ packages/nimbus/src/components/toggle-button/toggle-button.types.ts
@@ -0,0 +1,19 @@
+import type { ToggleButtonRootProps } from "./toggle-button.slots";
+import type { ToggleButtonProps as AriaToggleButtonProps } from "react-aria-components";
+
+/**
+ * Additional properties we want to exclude from the ToggleButton component.
+ * These are chakra-ui props we don't want exposed for non-polymorphic components.
+ */
+type ExcludedProps =
+  // chakra-ui props we don't want exposed
+  "css" | "colorScheme" | "unstyled" | "recipe" | "as" | "asChild";
+
+export interface ToggleButtonProps
+  extends AriaToggleButtonProps,
+    Omit<ToggleButtonRootProps, keyof AriaToggleButtonProps | ExcludedProps> {
+  /**
+   * Ref to the underlying button element
+   */
+  ref?: React.Ref<HTMLButtonElement>;
+}
