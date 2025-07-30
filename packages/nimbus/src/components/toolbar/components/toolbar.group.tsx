5a50ca0cfe8db2b29ccd432972fadfcdffb50784
Michael Salzmann <michael.salzmann@commercetools.com>
2025-07-30 14:19:26 +0200

    refactor(toolbar): enhance ToolbarGroup component by extracting style and functional props

--- packages/nimbus/src/components/toolbar/components/toolbar.group.tsx
+++ packages/nimbus/src/components/toolbar/components/toolbar.group.tsx
@@ -2,16 +2,18 @@ import { forwardRef } from "react";
 import { ToolbarGroup as ToolbarGroupSlot } from "../toolbar.slots.tsx";
 import type { ToolbarGroupProps } from "../toolbar.types.ts";
 import { Group } from "react-aria-components";
+import { extractStyleProps } from "@/utils/extractStyleProps";
 
 export const ToolbarGroup = ({
   ref: forwardedRef,
   ...props
 }: ToolbarGroupProps) => {
   const { children, ...rest } = props;
+  const [styleProps, functionalProps] = extractStyleProps(rest);
 
   return (
-    <ToolbarGroupSlot {...rest} ref={forwardedRef} role="group" asChild>
-      <Group>{children}</Group>
+    <ToolbarGroupSlot ref={forwardedRef} {...styleProps} asChild>
+      <Group {...functionalProps}>{children}</Group>
     </ToolbarGroupSlot>
   );
 };
