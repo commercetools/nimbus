fe723cd98ed0375a530448d7b5e76610051465b1
Ddouglasz <douglasegiemeh@gmail.com>
2025-07-31 17:10:07 +0200

    fix(data table): hide details button in disabled rows to prevent interaction

--- packages/nimbus/src/components/data-table/data-table.recipe.tsx
+++ packages/nimbus/src/components/data-table/data-table.recipe.tsx
@@ -87,6 +87,9 @@ export const dataTableRecipe = defineSlotRecipe({
         backgroundColor: "{colors.primary.4}",
       },
       "& .data-table-row-disabled": {
+        "& .data-table-row-details-button": {
+          display: "none",
+        },
         // layerStyle: "disabled",
         opacity: 0.8,
         cursor: "not-allowed",
