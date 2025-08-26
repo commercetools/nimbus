53affe708e991a31980eedd98a7b74142d07895b
Valorie <valorie.carli@commercetools.com>
2025-08-25 20:57:46 -0500

    chore(intl): first sample pass

---
+++ packages/nimbus/src/components/number-input/messages.ts
@@ -0,0 +1,14 @@
+import { defineMessages } from "react-intl";
+
+export default defineMessages({
+  decrement: {
+    id: "Nimbus.numberInput.decrement",
+    description: "aria-label for the decrement button in number input",
+    defaultMessage: "Decrement",
+  },
+  increment: {
+    id: "Nimbus.numberInput.increment",
+    description: "aria-label for the increment button in number input",
+    defaultMessage: "Increment",
+  },
+});
