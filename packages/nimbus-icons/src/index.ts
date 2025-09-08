56aaf1aa3265ea1f559d187df9c71f382ab41911
jaikamat <jaipkamat@gmail.com>
2025-09-08 15:05:31 -0400

    chore(nimbus): create custom high precision icon

--- packages/nimbus-icons/src/index.ts
+++ packages/nimbus-icons/src/index.ts
@@ -3,3 +3,4 @@ export * from "./material-icons";
 export { Figma } from "./custom-icons/figma";
 export { Github } from "./custom-icons/github";
 export { CommercetoolsCube } from "./custom-icons/commercetools-cube";
+export { HighPrecision } from "./custom-icons/high-precision";
