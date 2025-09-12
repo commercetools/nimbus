37e32dcd638c02c45378a42a66c4fd5ce9ca84c0
Byron Wall <byron.wall@commercetools.com>
2025-09-12 11:58:23 -0400

    feat(inline svg): use dompurify instead of custom parsing solution so that malicious/malformed attrs are removed from the svg but the svg is still displayed

--- packages/nimbus/src/components/inline-svg/inline-svg.stories.tsx
+++ packages/nimbus/src/components/inline-svg/inline-svg.stories.tsx
@@ -58,13 +58,14 @@ const multiColorSvg = `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/sv
 </svg>`;
 
 // Malicious SVG data for security testing
-const maliciousSvg = `<svg fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" onclick="alert('XSS')" onLoad="alert('XSS2')">
-  <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" onmouseover="alert('XSS4')"/>
-  <polyline points="7.5 4.21 12 6.81 16.5 4.21"/>
+const maliciousSvg = `<svg fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" onclick="alert('XSS')" onLoad="alert('XSS2')">
+  <path xlink:href="data:x,<script>alert('XSS5')</script>" d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" onmouseover="alert('XSS4')"/>
+  <polyline xlink:malicious="<script>alert('XSS6')</script>"points="7.5 4.21 12 6.81 16.5 4.21"/>
   <polyline points="7.5 19.79 7.5 14.6 3 12"/>
   <polyline points="21 12 16.5 14.6 16.5 19.79"/>
   <polyline points="3.27 6.96 12 12.01 20.73 6.96"/>
   <line x1="12" x2="12" y1="22.08" y2="12"/>
+  <a xlink:href="https://www.google.com/"/>
   <style>body { display: none; }</style>
   <script>alert('XSS3')</script>
 </svg>`;
@@ -237,6 +238,16 @@ export const SecurityTest: Story = {
       allElements.forEach((element) => {
         Array.from(element.attributes).forEach((attr) => {
           expect(attr.name.toLowerCase().startsWith("on")).toBe(false);
+          // The xlink on the `a` tag is legit, so it should be kept
+          if (
+            element.tagName === "a" &&
+            attr.name.toLowerCase().startsWith("xlink")
+          ) {
+            expect(attr.value).toBe("https://www.google.com/");
+          } else {
+            // The xlink on the `path` and `polyline` tags are malicious, so they should be removed
+            expect(attr.name.toLowerCase().startsWith("xlink")).toBe(false);
+          }
         });
       });
     });
