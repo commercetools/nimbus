41483b72924447e5a8b31bb84d759308a0704e33
jaikamat <jaipkamat@gmail.com>
2025-09-30 10:58:02 -0400

    chore(n): remove note

--- packages/nimbus/src/components/collapsible-motion/collapsible-motion.recipe.tsx
+++ packages/nimbus/src/components/collapsible-motion/collapsible-motion.recipe.tsx
@@ -8,10 +8,6 @@ export const collapsibleMotionSlotRecipe = defineSlotRecipe({
     trigger: {
       cursor: "pointer",
     },
-    // Content slot - basic container
-    content: {
-      // TODO: No animation styles - will be implemented later once RAC releases support for this PR: https://github.com/adobe/react-spectrum/pull/8867
-      // TODO: support minHeight feature parity with UI-Kit's CollapsibleMotion
-    },
+    content: {},
   },
 });
