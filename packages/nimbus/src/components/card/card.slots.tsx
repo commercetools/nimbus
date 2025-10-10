2ba5987a2e5fb1eb5afeaaef8a63d2965043ac41
jaikamat <jaipkamat@gmail.com>
2025-10-10 14:26:33 -0400

    chore(n): card

--- packages/nimbus/src/components/card/card.slots.tsx
+++ packages/nimbus/src/components/card/card.slots.tsx
@@ -1,37 +1,12 @@
-import {
-  type HTMLChakraProps,
-  type RecipeProps,
-  type UnstyledProp,
-  createSlotRecipeContext,
-} from "@chakra-ui/react/styled-system";
-
-import { cardRecipe } from "./card.recipe";
-
-/**
- * Base recipe props interface that combines Chakra UI's recipe props
- * with the unstyled prop option for the div element.
- */
-interface CardRecipeProps extends RecipeProps<"div">, UnstyledProp {}
-
-/**
- * Root props interface that extends Chakra's HTML props with our recipe props.
- * This creates a complete set of props for the root element, combining
- * HTML attributes, Chakra's styling system, and our custom recipe props.
- */
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface CardRootProps
-  extends HTMLChakraProps<"div", CardRecipeProps> {}
-
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface CardHeaderProps
-  extends HTMLChakraProps<"div", CardRecipeProps> {}
-
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface CardContentProps
-  extends HTMLChakraProps<"div", CardRecipeProps> {}
+import { createSlotRecipeContext } from "@chakra-ui/react";
+import type {
+  CardContentProps,
+  CardHeaderProps,
+  CardRootProps,
+} from "./card.types";
 
 const { withProvider, withContext } = createSlotRecipeContext({
-  recipe: cardRecipe,
+  key: "card",
 });
 
 /**
