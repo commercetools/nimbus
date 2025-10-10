f55bc6b293b9c4fbbfb7e65d8fa48152a8af0dcb
jaikamat <jaipkamat@gmail.com>
2025-10-10 15:04:06 -0400

    chore(n): checkbox

--- packages/nimbus/src/components/checkbox/checkbox.types.tsx
+++ packages/nimbus/src/components/checkbox/checkbox.types.tsx
@@ -1,24 +1,25 @@
+import type { HTMLChakraProps, SlotRecipeProps } from "@chakra-ui/react";
 import type { ReactNode, Ref } from "react";
-import type { CheckboxRootProps } from "./checkbox.slots";
 import type { CheckboxProps as RaCheckboxProps } from "react-aria-components";
 
-type CheckboxRecipeVariantProps = {
-  /**
-   * Size variant
-   * @default "md"
-   */
-  size?: "md";
-};
+type CheckboxRecipeProps = SlotRecipeProps<"checkbox">;
+
+export type CheckboxRootProps = Omit<
+  HTMLChakraProps<"label", CheckboxRecipeProps>,
+  keyof RaCheckboxProps
+> &
+  RaCheckboxProps;
+
+export type CheckboxLabelProps = HTMLChakraProps<"span">;
+
+export type CheckboxIndicatorProps = HTMLChakraProps<"span">;
 
 /**
  * Main props interface for the Checkbox component.
- * Extends CheckboxRecipeVariantProps to include both root props and variant props,
- * while adding support for React children.
+ * Extends CheckboxRootProps (which includes SlotRecipeProps and RaCheckboxProps),
+ * while adding support for React children and ref forwarding.
  */
-
-export type CheckboxProps = CheckboxRecipeVariantProps &
-  CheckboxRootProps &
-  RaCheckboxProps & {
-    ref?: Ref<RaCheckboxProps>;
-    children?: ReactNode;
-  };
+export type CheckboxProps = CheckboxRootProps & {
+  ref?: Ref<HTMLLabelElement>;
+  children?: ReactNode;
+};
