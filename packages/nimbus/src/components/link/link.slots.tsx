ca2d5ccd15086b3be1988df26f52825e8b38562c
jaikamat <jaipkamat@gmail.com>
2025-10-13 10:42:32 -0400

    chore(n): link

--- packages/nimbus/src/components/link/link.slots.tsx
+++ packages/nimbus/src/components/link/link.slots.tsx
@@ -1,26 +1,7 @@
-import {
-  type HTMLChakraProps,
-  type RecipeProps,
-  type UnstyledProp,
-  createRecipeContext,
-} from "@chakra-ui/react/styled-system";
+import { createRecipeContext } from "@chakra-ui/react/styled-system";
+import type { LinkRootProps } from "./link.types";
 
-import { linkRecipe } from "./link.recipe";
-
-/**
- * Base recipe props interface that combines Chakra UI's recipe props
- * with the unstyled prop option for the a element.
- */
-interface LinkRecipeProps extends RecipeProps<"a">, UnstyledProp {}
-
-/**
- * Root props interface that extends Chakra's HTML props with our recipe props.
- * This creates a complete set of props for the root element, combining
- * HTML attributes, Chakra's styling system, and our custom recipe props.
- */
-export type LinkRootProps = HTMLChakraProps<"a", LinkRecipeProps>;
-
-const { withContext } = createRecipeContext({ recipe: linkRecipe });
+const { withContext } = createRecipeContext({ key: "link" });
 
 /**
  * Root component that provides the styling context for the Link component.
--- packages/nimbus/src/components/link/link.types.tsx
+++ packages/nimbus/src/components/link/link.types.tsx
@@ -1,12 +1,21 @@
-import type { LinkRootProps } from "./link.slots";
+import type {
+  HTMLChakraProps,
+  RecipeProps,
+  UnstyledProp,
+} from "@chakra-ui/react";
 import type { AriaLinkOptions } from "react-aria";
 
-type LinkRecipeVariantProps = {
-  /** Size variant */
-  size?: "xs" | "sm" | "md";
-  /** FontColor variant */
-  fontColor?: "primary" | "inherit";
-};
+/**
+ * Base recipe props interface that combines Chakra UI's recipe props
+ * with the unstyled prop option for the a element.
+ */
+type LinkRecipeProps = RecipeProps<"link"> & UnstyledProp;
+/**
+ * Root props interface that extends Chakra's HTML props with our recipe props.
+ * This creates a complete set of props for the root element, combining
+ * HTML attributes, Chakra's styling system, and our custom recipe props.
+ */
+export type LinkRootProps = HTMLChakraProps<"a", LinkRecipeProps>;
 
 /**
  * Combines the root props with Chakra UI's recipe variant props.
@@ -16,8 +25,7 @@ type LinkRecipeVariantProps = {
  * Differences between LinkRootProps and LinkVariantProps necessitate
  * the use of Omit and Pick to ensure the correct props are passed
  */
-type LinkVariantProps = LinkRecipeVariantProps &
-  Omit<LinkRootProps, "onFocus" | "onBlur" | "onClick"> &
+type LinkVariantProps = Omit<LinkRootProps, "onFocus" | "onBlur" | "onClick"> &
   Pick<AriaLinkOptions, "onFocus" | "onBlur" | "onClick"> & {
     [key: `data-${string}`]: string;
   };
