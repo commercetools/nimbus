2200597fa892310a5f146fff345002294d0ca6db
tyler ford <tylerford@gmail.com>
2025-10-22 11:56:17 -0400

    chore(components): typecheck issues forcing my hand here yall

--- packages/nimbus/src/components/loading-spinner/loading-spinner.types.tsx
+++ packages/nimbus/src/components/loading-spinner/loading-spinner.types.tsx
@@ -18,7 +18,7 @@ type LoadingSpinnerRecipeProps = {
    * Color palette for the loading spinner
    * @default "primary"
    */
-  colorPalette?: RecipeProps<"loadingSpinner">["tone"];
+  colorPalette?: "primary" | "white" | undefined;
 } & UnstyledProp;
 
 // ============================================================
