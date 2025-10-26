d4a343b197a628779e8fd216c9d17459231540d8
Michael Salzmann <michael.salzmann@commercetools.com>
2025-10-26 14:50:07 +0100

    chore(index): remove use-toast export from hooks index file

--- packages/nimbus/src/hooks/index.ts
+++ packages/nimbus/src/hooks/index.ts
@@ -3,4 +3,3 @@ export * from "./use-copy-to-clipboard";
 export * from "./use-color-scheme";
 export * from "./use-color-mode";
 export * from "./use-color-mode-value";
-export * from "./use-toast";
