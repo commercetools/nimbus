2cdc384eb0cdeec82886ad83f496bcabc0a13018
Ddouglasz <douglasegiemeh@gmail.com>
2025-10-27 17:25:11 +0100

    refactor(data-table): reorganize i18n messages

--- packages/nimbus/src/components/data-table/data-table.i18n.ts
+++ packages/nimbus/src/components/data-table/data-table.i18n.ts
@@ -143,40 +143,40 @@ export const messages = defineMessages({
     description:
       "placeholder for the search input in the hidden columns section in the data table manager",
     defaultMessage: "Search...",
-    dataTable: {
-      id: "Nimbus.DataTable.dataTable",
-      description: "aria-label for the data table element",
-      defaultMessage: "Data table",
-    },
-    dataTableHeader: {
-      id: "Nimbus.DataTable.dataTableHeader",
-      description: "aria-label for the data table header element",
-      defaultMessage: "Data table header",
-    },
-    dataTableBody: {
-      id: "Nimbus.DataTable.dataTableBody",
-      description: "aria-label for the data table body element",
-      defaultMessage: "Data table body",
-    },
-    expandRows: {
-      id: "Nimbus.DataTable.expandRows",
-      description: "aria-label for the expand rows column header",
-      defaultMessage: "Expand rows",
-    },
-    pinRows: {
-      id: "Nimbus.DataTable.pinRows",
-      description: "aria-label for the pin rows column header",
-      defaultMessage: "Pin rows",
-    },
-    selectRow: {
-      id: "Nimbus.DataTable.selectRow",
-      description: "aria-label for the row selection checkbox",
-      defaultMessage: "Select row",
-    },
-    resizeColumn: {
-      id: "Nimbus.DataTable.resizeColumn",
-      description: "aria-label for the column resizer",
-      defaultMessage: "Resize column",
-    },
+  },
+  dataTable: {
+    id: "Nimbus.DataTable.dataTable",
+    description: "aria-label for the data table element",
+    defaultMessage: "Data table",
+  },
+  dataTableHeader: {
+    id: "Nimbus.DataTable.dataTableHeader",
+    description: "aria-label for the data table header element",
+    defaultMessage: "Data table header",
+  },
+  dataTableBody: {
+    id: "Nimbus.DataTable.dataTableBody",
+    description: "aria-label for the data table body element",
+    defaultMessage: "Data table body",
+  },
+  expandRows: {
+    id: "Nimbus.DataTable.expandRows",
+    description: "aria-label for the expand rows column header",
+    defaultMessage: "Expand rows",
+  },
+  pinRows: {
+    id: "Nimbus.DataTable.pinRows",
+    description: "aria-label for the pin rows column header",
+    defaultMessage: "Pin rows",
+  },
+  selectRow: {
+    id: "Nimbus.DataTable.selectRow",
+    description: "aria-label for the row selection checkbox",
+    defaultMessage: "Select row",
+  },
+  resizeColumn: {
+    id: "Nimbus.DataTable.resizeColumn",
+    description: "aria-label for the column resizer",
+    defaultMessage: "Resize column",
   },
 });
