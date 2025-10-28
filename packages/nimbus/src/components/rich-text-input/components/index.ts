761e308b95b9268e5f2c207ba1c9f532b36ac60d
jaikamat <jaipkamat@gmail.com>
2025-10-28 15:12:54 -0400

    feat(rich-text-input): restructure imports

---
+++ packages/nimbus/src/components/rich-text-input/components/index.ts
@@ -0,0 +1,3 @@
+export { FormattingMenu } from "./formatting-menu";
+export { RichTextEditor, type RichTextEditorRef } from "./rich-text-editor";
+export { RichTextToolbar } from "./rich-text-toolbar";
