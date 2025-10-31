71a1f92729b506d8ecfffe8fb75398999719ec75
Michael Salzmann <michael.salzmann@commercetools.com>
2025-10-31 10:38:23 +0100

    refactor(view-tabs): enhance tab navigation with URL state management

--- packages/nimbus/src/components/tabs/components/tabs.root.tsx
+++ packages/nimbus/src/components/tabs/components/tabs.root.tsx
@@ -1,8 +1,10 @@
+import { useSlotRecipe } from "@chakra-ui/react/styled-system";
 import { Tabs as RATabs } from "react-aria-components";
 import { TabsRootSlot } from "../tabs.slots";
 import type { TabsProps } from "../tabs.types";
 import { TabsList } from "./tabs.list";
 import { TabsPanels } from "./tabs.panels";
+import { extractStyleProps } from "@/utils";
 
 /**
  * # Tabs
@@ -11,24 +13,20 @@ import { TabsPanels } from "./tabs.panels";
  *
  * @supportsStyleProps
  */
-export const TabsRoot = ({
-  children,
-  orientation = "horizontal",
-  disabledKeys,
-  placement = "start",
-  size = "md",
-  tabs,
-  ...props
-}: TabsProps) => {
+export const TabsRoot = (props: TabsProps) => {
+  // Standard pattern: Split recipe variants
+  const recipe = useSlotRecipe({ key: "tabs" });
+  const [recipeProps, restRecipeProps] = recipe.splitVariantProps(props);
+
+  // Standard pattern: Extract style props
+  const [styleProps, functionalProps] = extractStyleProps(restRecipeProps);
+
+  // Separate component-specific props from React Aria props
+  const { children, tabs, ...raTabsProps } = functionalProps;
+
   return (
-    <TabsRootSlot
-      asChild
-      orientation={orientation}
-      placement={placement}
-      size={size}
-      {...props}
-    >
-      <RATabs disabledKeys={disabledKeys} {...tabs}>
+    <TabsRootSlot asChild {...recipeProps} {...styleProps}>
+      <RATabs {...raTabsProps}>
         {children || (
           <>
             <TabsList tabs={tabs} />
