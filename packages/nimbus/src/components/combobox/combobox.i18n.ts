7294db48c40cefd521481a5e3629bf7ac31032bb
Valorie <47182700+valoriecarli@users.noreply.github.com>
2026-01-20 10:45:01 -0600

    CRAFT-2061-Correct `Combobox` msgs (#925)
    * chore(intl): correct combobox msgs
    
    * fix(lint): make the linter happy

---
+++ packages/nimbus/src/components/combobox/combobox.i18n.ts
@@ -0,0 +1,24 @@
+import { defineMessages } from "react-intl";
+
+export const messages = defineMessages({
+  selectedValues: {
+    id: "Nimbus.ComboBox.selectedValues",
+    description: "aria-label for selected values tag group",
+    defaultMessage: "Selected values",
+  },
+  toggleOptions: {
+    id: "Nimbus.ComboBox.toggleOptions",
+    description: "aria-label for toggle button to open/close options",
+    defaultMessage: "Toggle options",
+  },
+  clearSelection: {
+    id: "Nimbus.ComboBox.clearSelection",
+    description: "aria-label for clear selection button in ComboBox",
+    defaultMessage: "Clear selection",
+  },
+  options: {
+    id: "Nimbus.ComboBox.options",
+    description: "aria-label for combobox menu options",
+    defaultMessage: "Options menu",
+  },
+});
