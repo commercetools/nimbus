3e2bc2f1f92fc42938eb6548d4a8324e63ce7005
Jai Kamat <jaipkamat@gmail.com>
2026-01-22 10:33:57 -0500

    chore(nimbus): fix `InlineSvg` react warnings (#923)
    chore(nimbus): fix inline svg component's react warnings

--- packages/nimbus/src/components/inline-svg/hooks/use-inline-svg.ts
+++ packages/nimbus/src/components/inline-svg/hooks/use-inline-svg.ts
@@ -50,8 +50,8 @@ export function useInlineSvg(data: string) {
     // Preserve all attributes from the sanitized SVG element
     // Security: Only attributes that passed sanitization are included
     for (const attr of Array.from(svgEl.attributes)) {
-      // Convert kebab-case to camelCase for React compatibility
-      const reactAttrName = attr.name.replace(/-([a-z])/g, (_, letter) =>
+      // Convert kebab-case and namespace prefixes to camelCase for React compatibility
+      const reactAttrName = attr.name.replace(/[-:]([a-z])/g, (_, letter) =>
         letter.toUpperCase()
       );
       attrs[reactAttrName] = attr.value;
