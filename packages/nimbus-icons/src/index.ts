ae3e383e7abf411a1a1bc46c35701cf6c04b08f0
Valorie <valorie.carli@commercetools.com>
2026-02-17 11:31:53 -0600

    chore(icons): fix comment

--- packages/nimbus-icons/src/index.ts
+++ packages/nimbus-icons/src/index.ts
@@ -4,5 +4,5 @@ export { Figma } from "./custom-icons/figma";
 export { Github } from "./custom-icons/github";
 export { CommercetoolsCube } from "./custom-icons/commercetools-cube";
 export { HighPrecision } from "./custom-icons/high-precision";
-// Override the imported Material Icon CheckCircle with our custom filled version (from Material) so we have one filled, one outlined version.
+// Override buggy CheckCircle with correct Material filled version
 export { default as CheckCircle } from "./custom-icons/check-circle";
