d13060d7b5ad336d70acf33b78139fe995f4dd3b
Valorie <valorie.carli@commercetools.com>
2026-02-17 11:19:25 -0600

    feat(icons): add custom material CheckCircle icon to override importn

--- packages/nimbus-icons/src/index.ts
+++ packages/nimbus-icons/src/index.ts
@@ -4,3 +4,5 @@ export { Figma } from "./custom-icons/figma";
 export { Github } from "./custom-icons/github";
 export { CommercetoolsCube } from "./custom-icons/commercetools-cube";
 export { HighPrecision } from "./custom-icons/high-precision";
+// Override the imported Material Icon CheckCircle with our custom filled version (from Material) so we have one filled, one outlined version.
+export { default as CheckCircle } from "./custom-icons/check-circle";
