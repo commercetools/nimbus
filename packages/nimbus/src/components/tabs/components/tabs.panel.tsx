50d506a03a39a58403b7a265012804fd6ed0f5f1
Byron Wall <byron.wall@commercetools.com>
2026-03-04 14:10:43 -0500

    fix(tab types): import from chakra module instead of barrel

--- packages/nimbus/src/components/tabs/components/tabs.panel.tsx
+++ packages/nimbus/src/components/tabs/components/tabs.panel.tsx
@@ -1,4 +1,3 @@
-import { forwardRef } from "react";
 import { TabPanel as RATabPanel } from "react-aria-components";
 import { TabsPanelSlot } from "../tabs.slots";
 import type { TabPanelProps } from "../tabs.types";
@@ -7,20 +6,20 @@ import type { TabPanelProps } from "../tabs.types";
  * # TabPanel
  *
  * An individual tab panel that displays panelContent when its corresponding tab is selected.
+ *
+ * @supportsStyleProps
  */
-export const TabsPanel = forwardRef<HTMLDivElement, TabPanelProps>(
-  ({ children, ...props }, ref) => {
-    // Ensure TabPanel always has children
-    if (!children) {
-      throw new Error("Tabs.Panel: children prop is required");
-    }
-
-    return (
-      <TabsPanelSlot asChild ref={ref} {...props}>
-        <RATabPanel>{children}</RATabPanel>
-      </TabsPanelSlot>
-    );
+export const TabsPanel = ({ children, ref, ...props }: TabPanelProps) => {
+  // Ensure TabPanel always has children
+  if (!children) {
+    throw new Error("Tabs.Panel: children prop is required");
   }
-);
+
+  return (
+    <TabsPanelSlot asChild ref={ref} {...props}>
+      <RATabPanel>{children}</RATabPanel>
+    </TabsPanelSlot>
+  );
+};
 
 TabsPanel.displayName = "Tabs.Panel";
