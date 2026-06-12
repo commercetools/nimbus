e4df38d212d8a4c4b15a8affd3fe4e6742530e3a
Byron Wall <byron.wall@commercetools.com>
2026-06-11 22:38:51 -0400

    fix: add dist-* to eslint global ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -43,7 +43,12 @@ export default tseslint.config(
    * Global ignores for common build and dependency directories
    */
   {
-    ignores: ["**/node_modules/**", "**/dist/**", "**/storybook-static/**"],
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/dist-*/**",
+      "**/storybook-static/**",
+    ],
   },
   /**
    * Base ESLint recommended rules
