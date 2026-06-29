ef6067b2a58d5a4450486982cf16c8598f9bb4ca
Valorie <valorie.carli@commercetools.com>
2026-06-29 13:56:38 -0500

    chore(chromatic): strip workflow down to basics and revert trials for Code exports

--- packages/nimbus/src/components/code/code.slots.tsx
+++ packages/nimbus/src/components/code/code.slots.tsx
@@ -3,15 +3,6 @@ import type { CodeRootSlotProps } from "./code.types";
 
 const { withContext } = createRecipeContext({ key: "nimbusCode" });
 
-/**
- * # Code
- *
- * renders code blocks
- *
- * @see {@link https://nimbus-documentation.vercel.app/components/typography/code}
- *
- * @experimental This component is experimental and may change or be removed in future versions.
- */
 export const CodeRoot: React.FC<CodeRootSlotProps> = withContext<
   HTMLElement,
   CodeRootSlotProps
--- packages/nimbus/src/components/code/code.tsx
+++ packages/nimbus/src/components/code/code.tsx
@@ -1,1 +1,12 @@
-export { CodeRoot as Code } from "./code.slots";
+import { CodeRoot } from "./code.slots";
+
+/**
+ * # Code
+ *
+ * renders code blocks
+ *
+ * @see {@link https://nimbus-documentation.vercel.app/components/typography/code}
+ *
+ * @experimental This component is experimental and may change or be removed in future versions.
+ */
+export const Code: typeof CodeRoot = CodeRoot;
