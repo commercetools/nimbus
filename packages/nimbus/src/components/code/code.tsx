05e29898235cdac967278712ca8a58a590da3a5e
Valorie <valorie.carli@commercetools.com>
2026-06-29 12:02:11 -0500

    chore(code): update Code component documentation and export structure

--- packages/nimbus/src/components/code/code.tsx
+++ packages/nimbus/src/components/code/code.tsx
@@ -1,12 +1,1 @@
-import { CodeRoot } from "./code.slots";
-
-/**
- * # Code
- *
- * renders code blocks
- *
- * @see {@link https://nimbus-documentation.vercel.app/components/typography/code}
- *
- * @experimental This component is experimental and may change or be removed in future versions.
- */
-export const Code: typeof CodeRoot = CodeRoot;
+export { CodeRoot as Code } from "./code.slots";
